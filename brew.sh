22ad91af850c552b1ab64a02fb6c4936a2e644cb
Håvard Heggheim <havheg90@gmail.com>
2015-04-29 22:02:06 +0200

    first commit

---
+++ brew.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Install GNU core utilities (those that come with OS X are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+
+# Install `wget` with IRI support.
+brew install wget --with-iri
+
+#install other
+brew install git
+brew install tree
+
+# Install Node.js. Note: this installs `npm` too, using the recommended
+# installation method.
+brew install node
+
+# Remove outdated versions from the cellar.
+brew cleanup
