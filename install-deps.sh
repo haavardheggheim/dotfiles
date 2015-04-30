e0942d3764d8a56951e36a827f9a57d5aabd09c7
Håvard Heggheim <havheg90@gmail.com>
2015-04-30 07:23:42 +0200

    fixing sublime symlinking

--- install-deps.sh
+++ install-deps.sh
@@ -1,2 +1,2 @@
 # Set symlink for Sublime Text
-ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl
+sudo ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/bin/subl
